53b2228dc6fa4ce5bdff0eadbf9b7dc072f514af
rachel_giacobozzi <rgiacobozzi@gmail.com>
2014-03-29 10:17:58 -0400

    created new rails app wine_review

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WineReview::Application.config.secret_key_base = 'd9ef7c2ed5b43ac0c570650cc1d6803303cbea194840c6d41d1ce1fac2abc3f6ae479c2046147171c0c1670781ef38260883563d3944c249b1d67bdd4d899f6d'
